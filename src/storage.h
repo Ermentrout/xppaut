d16b1d3f40308d1ee58de62506ea9ca56698a703
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-17 17:00:42 +0100

    Removed externs from dormpri.c

--- src/storage.h
+++ src/storage.h
@@ -1,10 +1,14 @@
-#ifndef _storage_h_
-#define _storage_h_
+#ifndef XPPAUT_STORAGE_H
+#define XPPAUT_STORAGE_H
 
+/* --- Data --- */
+extern double *WORK;
+
+/* --- Functions --- */
 void init_alloc_info(void);
 void alloc_meth(void);
 void init_stor(int nrow, int ncol);
 void free_storage(int ncol);
 int reallocstor(int ncol,int nrow);
 
-#endif
+#endif /* XPPAUT_STORAGE_H */
