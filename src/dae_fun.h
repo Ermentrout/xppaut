850f9a75da2d7850f2a0520701dfc03acb7353f9
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-19 13:22:22 +0100

    Moved typedefs from dae_fun.c to header

--- src/dae_fun.h
+++ src/dae_fun.h
@@ -1,6 +1,26 @@
-#ifndef _dae_fun_h_
-#define _dae_fun_h_
+#ifndef XPPAUT_DAE_FUN_H
+#define XPPAUT_DAE_FUN_H
 
+/* --- Types --- */
+typedef struct {
+  double *work;
+  int *iwork;
+  int status;
+} DAEWORK;
+
+typedef struct {
+  char name[12],*rhs;
+  int *form;
+  int index;
+  double value,last;
+} SOLV_VAR;
+
+typedef struct {
+  char *rhs;
+  int *form;
+} DAE_EQN;
+
+/* --- Functions --- */
 int add_svar(char *name, char *rhs);
 int add_svar_names(void);
 int add_aeqn(char *rhs);
@@ -14,4 +34,4 @@ void do_daes(void);
 int solve_dae(void);
 void get_new_guesses(void);
 
-#endif
+#endif /* XPPAUT_DAE_FUN_H */
