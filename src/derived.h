e19a70dbbe8399072a6a87e77b7fc71d79bc22d9
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-19 13:28:40 +0100

    Moved typedef from derived.c to header

--- src/derived.h
+++ src/derived.h
@@ -1,11 +1,18 @@
-#ifndef _derived_h
-#define _derived_h
+#ifndef XPPAUT_DERIVED_H
+#define XPPAUT_DERIVED_H
 
 
+/* --- Types --- */
+typedef struct {
+  int index,*form;
+  char *rhs;
+  double value;
+} DERIVED;
+
+/* --- Functions --- */
 void free_derived(void);
 int compile_derived(void);
 void evaluate_derived(void);
 int add_derived(char *name, char *rhs);
 
- 
-#endif
+#endif /* XPPAUT_DERIVED_H */
