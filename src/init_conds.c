7c2b12b7869791a36d11079319b73315012a2e01
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-21 20:27:19 +0100

    Remove unused macros from init_conds.c

--- src/init_conds.c
+++ src/init_conds.c
@@ -66,19 +66,16 @@
 #define WRITEM 0
 #define MAX_LEN_SBOX 25
 
-#define MAXLINES 5000
 #define PARAMBOX 1
 #define ICBOX 2
 #define DELAYBOX 3
 #define BCBOX 4
-#define BOXEVENT  (ButtonPressMask 	|\
-		KeyPressMask		|\
-		ExposureMask		|\
-		StructureNotifyMask	|\
-		LeaveWindowMask		|\
-		EnterWindowMask)
-
-#define BOXDONE -2
+#define BOXEVENT   (ButtonPressMask 	|\
+                    KeyPressMask		|\
+                    ExposureMask		|\
+                    StructureNotifyMask	|\
+                    LeaveWindowMask		|\
+                    EnterWindowMask)
 
 #define EDIT_WAIT 0
 #define EDIT_NEXT 1
