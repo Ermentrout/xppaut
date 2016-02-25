c48d2fd8c85879a56c12a535a05f2167923a6a26
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-25 14:47:23 +0100

    Modernize do_fit.ch

--- src/do_fit.h
+++ src/do_fit.h
@@ -3,12 +3,12 @@
 
 /* --- Types --- */
 typedef struct {
-  char file[25];
-  char varlist[25],collist[25];
-  char parlist1[25],parlist2[25];
-  int dim,npars,nvars,npts,maxiter;
-  int icols[50],ipar[50],ivar[50];
-  double tol,eps;
+	char file[25];
+	char varlist[25],collist[25];
+	char parlist1[25],parlist2[25];
+	int dim,npars,nvars,npts,maxiter;
+	int icols[50],ipar[50],ivar[50];
+	double tol,eps;
 } FITINFO;
 
 /* --- Functions --- */
