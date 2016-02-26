ad479f69949da61d57f661e9387d647e23dc0301
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-26 21:27:06 +0100

    Modernize stiff.ch

--- src/stiff.h
+++ src/stiff.h
@@ -1,8 +1,7 @@
-#ifndef _stiff_h_
-#define _stiff_h_
+#ifndef XPPAUT_STIFF_H
+#define XPPAUT_STIFF_H
 
-
-/* stiff.c */
+/* --- Functions --- */
 void jacobn(double x, double *y, double *dfdx, double *dermat, double eps, double *work, int n);
 int adaptive(double *ystart, int nvar, double *xs, double x2, double eps, double *hguess, double hmin, double *work, int *ier, double epjac, int iflag, int *jstart);
 int gadaptive(double *ystart, int nvar, double *xs, double x2, double eps, double *hguess, double hmin, double *work, int *ier, double epjac, int iflag, int *jstart);
@@ -10,6 +9,5 @@ int stiff(double y[], double dydx[], int n, double *x, double htry, double eps, 
 int rkqs(double *y, double *dydx, int n, double *x, double htry, double eps, double *yscal, double *hdid, double *hnext, double *work, int *ier);
 void rkck(double *y, double *dydx, int n, double x, double h, double *yout, double *yerr, double *work);
 
-
-#endif
+#endif /* XPPAUT_STIFF_H */
 
