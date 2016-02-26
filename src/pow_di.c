1e8c208e3f9261fae7c5cd114ff2ed60c9aa1080
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-26 20:31:58 +0100

    Change whitespace in pow_*.c

--- src/pow_di.c
+++ src/pow_di.c
@@ -6,30 +6,29 @@ double pow_di(ap, bp) doublereal *ap; integer *bp;
 double pow_di(doublereal *ap, integer *bp)
 #endif
 {
-double pow, x;
-integer n;
-unsigned long u;
+    double pow, x;
+    integer n;
+    unsigned long u;
 
-pow = 1;
-x = *ap;
-n = *bp;
+    pow = 1;
+    x = *ap;
+    n = *bp;
 
-if(n != 0)
-	{
-	if(n < 0)
-		{
-		n = -n;
-		x = 1/x;
-		}
-	for(u = n; ; )
-		{
-		if(u & 01)
-			pow *= x;
-		if(u >>= 1)
-			x *= x;
-		else
-			break;
-		}
-	}
-return(pow);
+    if(n != 0) {
+        if(n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        for(u = n; ; ) {
+            if(u & 01) {
+                pow *= x;
+            }
+            if(u >>= 1) {
+                x *= x;
+            } else {
+                break;
+            }
+        }
+    }
+    return(pow);
 }
