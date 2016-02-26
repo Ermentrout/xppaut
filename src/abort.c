a543d2e2e8af5c8e2dcb5ce637c93f8bfe500ac3
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-02-26 22:10:25 +0100

    UPdate coding style in abort.c

--- src/abort.c
+++ src/abort.c
@@ -11,58 +11,48 @@
 #include "many_pops.h"
 #include "mykeydef.h"
 
-int get_command_width()
-{
-	int x,y;
-	unsigned int w,h,bw,de;
+int get_command_width(void) {
+    int x,y;
+    unsigned int w,h,bw,de;
 
-	Window root;
-	XGetGeometry(display,command_pop,&root,&x,&y,&w,&h,&bw,&de);
-	XClearWindow(display,command_pop);
-	return(w);
+    Window root;
+    XGetGeometry(display,command_pop,&root,&x,&y,&w,&h,&bw,&de);
+    XClearWindow(display,command_pop);
+    return(w);
 }
 
 void plot_command(int nit, int icount, int cwidth) {
-	int i;
-	float dx;
-	if(nit==0)
-		return;
+    int i;
+    float dx;
+    if(nit==0) {
+        return;
+    }
+    dx=(float)icount*(float)cwidth/(float)nit;
+    i=(int)dx;
 
-	dx=(float)icount*(float)cwidth/(float)nit;
-	i=(int)dx;
-
-	XDrawPoint(display,command_pop,gc,i,5);
+    XDrawPoint(display,command_pop,gc,i,5);
 }
 
 
-int my_abort()
-{
-	int ch;
-	XEvent event;
-	while(XPending(display)>0){
-		XNextEvent(display,&event);
-		if(check_ani_pause(event)==ESC)
-			return(ESC);
-		switch(event.type){
-		case Expose: do_expose(event);
-			break;
-		case ButtonPress:
-			break;
-		case KeyPress:
-			ch=get_key_press(&event);
-			return(ch);
-
-		}
-		return(0);
-	}
-	return(64);
+int my_abort(void) {
+    int ch;
+    XEvent event;
+    while(XPending(display)>0) {
+        XNextEvent(display,&event);
+        if(check_ani_pause(event)==ESC) {
+            return(ESC);
+        }
+        switch(event.type) {
+        case Expose:
+            do_expose(event);
+            break;
+        case ButtonPress:
+            break;
+        case KeyPress:
+            ch=get_key_press(&event);
+            return(ch);
+        }
+        return(0);
+    }
+    return(64);
 }
-
-
-
-
-
-
-
-
-
