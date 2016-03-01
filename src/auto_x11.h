91ee955ff4f896c68f31ba4c39786352093624e4
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-03-01 20:56:42 +0100

    Minimize auto_x11 API

--- src/auto_x11.h
+++ src/auto_x11.h
@@ -3,12 +3,6 @@
 
 #include <X11/Xlib.h>
 
-/* --- Types --- */
-typedef struct  {
-	Window canvas, axes,numerics,grab,next,run,clear,redraw,base,per;
-	Window info,param,file,abort,stab,hint,kill;
-} AUTOWIN;
-
 /* --- Data --- */
 extern int AutoRedrawFlag;
 extern int mark_flag;
@@ -18,42 +12,34 @@ extern int mark_ipts;
 
 /* --- Functions --- */
 void ALINE(int a, int b, int c, int d);
-void DLINE(double a, double b, double c, double d);
 void ATEXT(int a, int b, char *c);
-void clr_stab(void);
+void auto_button(XEvent ev);
+void auto_enter(Window w, int v);
+void auto_get_info( int *n, char *pname);
+void auto_keypress(XEvent ev, int *used);
+void auto_motion(XEvent ev);
+int auto_pop_up_list(char *title, char **list, char *key, int n, int max, int def, int x, int y, char **hints, char *httxt);
+int auto_rubber(int *i1, int *j1, int *i2, int *j2, int flag);
+void auto_scroll_window(void);
+void auto_set_mark(int i);
 void auto_stab_line(int x, int y, int xp, int yp);
-void clear_auto_plot(void);
-void redraw_auto_menus(void);
-void traverse_diagram(void);
-void clear_auto_info(void);
-void draw_auto_info(char *bob, int x, int y);
-void refreshdisplay(void);
+void autobw(void);
+void autocol(int col);
 int byeauto_(int *iflag);
 void Circle(int x, int y, int r);
-void autocol(int col);
-void autobw(void);
-int auto_rubber(int *i1, int *j1, int *i2, int *j2, int flag);
-int auto_pop_up_list(char *title, char **list, char *key, int n, int max, int def, int x, int y, char **hints, char *httxt);
-void MarkAuto(int x,int y);
-void XORCross(int x, int y);
+void clear_auto_info(void);
+void clear_auto_plot(void);
+void clr_stab(void);
+void display_auto(Window w);
+void DLINE(double a, double b, double c, double d);
+void do_auto_range(void);
+void draw_auto_info(char *bob, int x, int y);
 void FillCircle(int x, int y, int r);
 void LineWidth(int wid);
-void auto_motion(XEvent ev);
-void display_auto(Window w);
-Window lil_button(Window root, int x, int y, char *name);
 void make_auto(char *wname, char *iname);
+void redraw_auto_menus(void);
+void refreshdisplay(void);
 void resize_auto_window(XEvent ev);
-void a_msg(int i, int v);
-void auto_enter(Window w, int v);
-void auto_button(XEvent ev);
-void auto_kill(void);
-void auto_keypress(XEvent ev, int *used);
-int query_special(char* title,char *nsymb);
-void clear_msg(void);
-void find_point(int ibr,int pt);
-void auto_get_info( int *n, char *pname);
-void auto_set_mark(int i);
-void do_auto_range(void);
-void RedrawMark(void);
+void traverse_diagram(void);
 
 #endif /* XPPAUT_AUTO_X11_H */
