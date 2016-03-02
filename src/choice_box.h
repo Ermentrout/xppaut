3a50620cbb3fac9e50dba028919b16cc86347ee6
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-03-02 10:19:58 +0100

    Remove unused choice_box.ch

--- src/choice_box.h
+++
@@ -1,9 +0,0 @@
-#ifndef XPPAUT_CHOICE_BOX_H
-#define XPPAUT_CHOICE_BOX_H
-
-#include <X11/Xlib.h>
-#include "struct.h"
-
-void display_choice(Window w, CHOICE_BOX p);
-
-#endif /* XPPAUT_CHOICE_BOX_H */
