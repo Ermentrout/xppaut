070bfbe30a540acc4c0a59a6d1c7630c7ee77429
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-03-02 10:27:51 +0100

    Minimize color.ch API

--- src/color.h
+++ src/color.h
@@ -13,17 +13,12 @@ extern int custom_color;
 extern int periodic;
 
 /* --- Functions --- */
-void tst_color(Window w);
-void set_scolor(int col);
-void set_color(int col);
-void make_cmaps(int *r, int *g, int *b, int n, int type);
-int rfun(double y, int per);
-int gfun(double y, int per);
-int bfun(double y, int per);
-void NewColormap(int type);
+int ColorMap(int i);
 void get_ps_color(int i, float *r, float *g, float *b);
 void get_svg_color(int i,int *r,int *g,int *b);
 void MakeColormap(void);
-int ColorMap(int i);
+void NewColormap(int type);
+void set_scolor(int col);
+void set_color(int col);
 
 #endif /* XPPAUT_COLOR_H */
