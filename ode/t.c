54125a16e4450e4b1cb44465cccd56fe7257a004
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-03-07 17:27:17 +0100

    Fix wrong usage of printf in t.c

--- ode/t.c
+++ ode/t.c
@@ -1,12 +1,13 @@
 #include <math.h>
-/*  
+#include <stdio.h>
+/*
  some example functions
 */
- 
+
 lv(double *in,double *out,int nin,int nout,double *var,double *con)
 {
   if(con[2]<.5) {
-	  printf("Here ya go \n");
+	  printf("%s", "Here ya go \n");
   }
   out[0]=-in[0];
   con[2]=1;
